fd871b0d74867c4a3829cb20295f8494cd70292a
Matheus Lima <matheus.lima@ccc.ufcg.edu.br>
2022-10-10 11:36:41 -0300

    fix

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import styles from "./footer.module.css";
 export function Footer() {
     return (
         <div className={styles.wrapper}>
-            <h3>Entre em contato!</h3>
+            <h3>Nossas Redes Sociais!</h3>
             <div className={styles.buttonsContainer}>
                 <button>
                     <TwitterLogo size={35} />
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import styles from "./projects.module.css";
+import logoKonkin from "../../assets/logoKonkin.png";
+import logoGazeta from "../../assets/logoGazeta.png";
+import logoClubeDaLiberdade from "../../assets/logoCL.png";
 
 export function Projects() {
     return (
         <>
             <div className={styles.wrapper}>
                 <div className={styles.container}>
-                    <h3>Editora Konkin:</h3>
+                    <a href="https://konkin.com.br/" target="_blank">
+                        <img src={logoKonkin} alt="" />
+                    </a>
                     <p>
                         A <b>Editora Konkin</b> tem o objetivo de disponibilizar
                         toda a grande literatura libertária internacional em
@@ -15,7 +20,9 @@ export function Projects() {
                     </p>
                 </div>
                 <div className={styles.container}>
-                    <h3>Gazeta Libertária:</h3>
+                    <a href="https://konkin.com.br/" target="_blank">
+                        <img src={logoGazeta} alt="" />
+                    </a>
                     <p>
                         A <b>Gazeta Libertária</b> é um jornal online
                         comprometido em trazer uma visão libertária sobre
@@ -25,7 +32,9 @@ export function Projects() {
                     </p>
                 </div>
                 <div className={styles.container}>
-                    <h3>Clube da Liberdade:</h3>
+                    <a href="https://konkin.com.br/" target="_blank">
+                        <img src={logoClubeDaLiberdade} alt="" />
+                    </a>
                     <p>
                         O <b>Clube da Liberdade</b> é uma plataforma com cursos
                         exclusivos sobre direito, economia, filosofia,
